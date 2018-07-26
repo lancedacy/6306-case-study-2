60a6a4a11208ac9e38c180e086ae8a42838f65ca
Lance Dacy <ldacy@smu.edu>
2018-07-26 11:34:22 -0600

    Updated with Iris Test Data

--- Scratch/CS_Lance_RandomForest.R
+++ Scratch/CS_Lance_RandomForest.R
@@ -9,3 +9,17 @@ plot(iris[,3], iris[,4], pch=21, xlab=names(iris)[3], ylab=names(iris)[4],
      bg=c("red", "blue", "green")[as.numeric(factor(iris$Species))],
      main="Iris Data with Prototypes")
 points(iris.p[,3], iris.p[,4], pch=21, cex=2, bg=c("red", "blue", "green"))
+
+#Another example
+rf1 <- randomForest(Species ~ ., iris, ntree=50, norm.votes=FALSE)
+rf2 <- randomForest(Species ~ ., iris, ntree=50, norm.votes=FALSE)
+rf3 <- randomForest(Species ~ ., iris, ntree=50, norm.votes=FALSE)
+rf.all <- combine(rf1, rf2, rf3)
+print(rf.all)
+
+## Look at the third trees in the forest.
+getTree(randomForest(iris[,-5], iris[,5], ntree=10), 3, labelVar=TRUE)
+
+iris.rf <- randomForest(Species ~ ., iris, ntree=50, norm.votes=FALSE)
+iris.rf <- grow(iris.rf, 50)
+print(iris.rf)
